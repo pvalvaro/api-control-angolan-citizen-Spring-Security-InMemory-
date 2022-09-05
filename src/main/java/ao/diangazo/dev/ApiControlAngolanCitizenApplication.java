f5fadc362d72ad959a60a80437315ce457fec64a
pvalvaro <pvalvaro25@gmail.com>
2022-09-05 13:15:30 -0300

    first commit

---
+++ src/main/java/ao/diangazo/dev/ApiControlAngolanCitizenApplication.java
@@ -0,0 +1,13 @@
+package ao.diangazo.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControlAngolanCitizenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControlAngolanCitizenApplication.class, args);
+	}
+
+}
